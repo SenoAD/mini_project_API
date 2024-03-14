0bbeba09c2773b23d19ad93cd4bce56658757e99
Seno Aji Darmawan <BSI80209@bsi.co.id>
2024-03-14 15:08:46 +0700

    first commit

---
+++ CrudServiceCategory/Controllers/ArticlesController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyWebFormApp.BLL.DTOs;
+using MyWebFormApp.BLL.Interfaces;
+
+namespace CrudServiceCategory.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticlesController : ControllerBase
+    {
+        private readonly IArticleBLL _articleBLL;
+
+        public ArticlesController(IArticleBLL articleBLL)
+        {
+            _articleBLL = articleBLL;
+        }
+        [HttpDelete]
+        public void Delete(int id) 
+        { 
+            _articleBLL.Delete(id);
+        }
+        [HttpGet("GetArticleByCategory")]
+        public IEnumerable<ArticleDTO> GetArticleByCategory(int categoryId) 
+        { 
+            var DTOarticle = _articleBLL.GetArticleByCategory(categoryId);
+            return DTOarticle;
+        }
+
+        [HttpGet("GetArticleById")]
+        public ArticleDTO GetArticleById(int id)
+        {
+            var DTOarticle = _articleBLL.GetArticleById(id);
+            return DTOarticle;
+        }
+
+        [HttpGet("GetArticleWithCategory")]
+        public IEnumerable<ArticleDTO> GetArticleWithCategory()
+        {
+            var DTOarticle = _articleBLL.GetArticleWithCategory();
+            return DTOarticle;
+        }
+
+        [HttpGet("GetCountArticles")]
+        public int GetCountArticles()
+        {
+            return _articleBLL.GetCountArticles();
+        }
+
+        [HttpGet("GetWithPaging")]
+        public IEnumerable<ArticleDTO> GetWithPaging(int categoryId, int pageNumber, int pageSize)
+        {
+            return _articleBLL.GetWithPaging(categoryId, pageNumber, pageSize);
+        }
+
+        [HttpPost]
+        public void Insert(ArticleCreateDTO articleDto)
+        {
+            _articleBLL.Insert(articleDto);
+        }
+
+        [HttpPost("InsertWithIdentity")]
+        public int InsertWithIdentity(ArticleCreateDTO articleDto)
+        {
+            return _articleBLL.InsertWithIdentity(articleDto);
+        }
+
+        [HttpPut]
+        public void Update(ArticleUpdateDTO article)
+        {
+            _articleBLL.Update(article);
+        }
+
+
+    }
+}
---
+++ CrudServiceCategory/Controllers/CategoriesController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyWebFormApp.BLL.DTOs;
+using MyWebFormApp.BLL.Interfaces;
+using MyWebFormApp.BLL;
+
+namespace CrudServiceCategory.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryBLL _categoryBLL;
+
+        public CategoriesController(ICategoryBLL categoryBLL)
+        {
+            _categoryBLL = categoryBLL;
+        }
+
+        [HttpGet]
+        public IEnumerable<CategoryDTO> Get() 
+        {
+            var categories = _categoryBLL.GetAll();
+            return categories;
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            _categoryBLL.Delete(id);
+        }
+
+        [HttpGet("{id}")]
+        public CategoryDTO GetById(int id) 
+        { 
+            var DTPCategory = _categoryBLL.GetById(id);
+            return DTPCategory;
+        }
+
+        [HttpGet("GetByName/{name}")]
+        public IEnumerable<CategoryDTO> GetByName(string name) 
+        { 
+            var DTPCategory = _categoryBLL.GetByName(name);
+            return DTPCategory;
+        }
+
+        [HttpGet("GetCountCategories")]
+        public int GetCountCategories(string name)
+        {
+            return _categoryBLL.GetCountCategories(name);
+        }
+
+        [HttpGet("{pageNumber}/{pageSize}/name")]
+        public IEnumerable<CategoryDTO> GetWithPaging(int pageNumber, int pageSize, string name)
+        {
+            var DTPCategory = _categoryBLL.GetWithPaging(pageNumber, pageSize, name);
+            return DTPCategory;
+        }
+
+        [HttpPost]
+        public void Insert(CategoryCreateDTO entity)
+        {
+            _categoryBLL.Insert(entity);
+        }
+        [HttpPut]
+        public void Update(CategoryUpdateDTO entity)
+        {
+            _categoryBLL.Update(entity);
+        }
+    }
+}
